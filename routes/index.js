ae56aeb15c09dc759aa87b6801239b8beb577526
Trevor Scheuermann <trevorsch1@gmail.com>
2015-06-26 11:38:15 -0600

    working product

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var unirest = require('unirest');
+var key = process.env.PIVOTAL_API;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  unirest.get('https://www.pivotaltracker.com/services/v5/projects/1374576/stories')
+  .header('X-TrackerToken', key)
+  .end(function (response){
+    res.render('index', {stories: response.body});
+  });
+});
+
+
+module.exports = router;
